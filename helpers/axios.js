1cb2fbb0e847ffac12800a8ad5066ca44e070db6
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-17 00:04:04 +0300

    axios updated with prodUrl.

--- helpers/axios.js
+++ helpers/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { localURL, prodURL } from "./constants";
 
 const instance = axios.create({
-  baseURL: localURL
+  baseURL: prodURL
 })
 
 export default instance;
