6cbf5d44c120467e5e190fbfbc0a00e4db5c8eab
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-03-22 23:57:11 +0300

    add loading ui

--- screens/AllNews.js
+++ screens/AllNews.js
@@ -1,26 +1,29 @@
 import axios from "../helpers/axios";
 import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 import BottomNews from "../components/BottomNews";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const AllNews = ({navigation}) => {
+const AllNews = ({ navigation }) => {
   const [search, setSearch] = useState("");
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getSearchResults = () => {
+    setLoading(true);
     axios
       .get("api/article/search?search=" + search)
       .then((response) => setArticles(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     const unsubscribe = getSearchResults();
 
     return unsubscribe;
-  },[])
+  }, []);
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -31,11 +34,20 @@ const AllNews = ({navigation}) => {
           handleClick={getSearchResults}
         />
         <Text style={styles.text}>All News</Text>
-        <FlatList
-          data={articles}
-          renderItem={() => <BottomNews navigation={navigation} />}
-          pagingEnabled
-        />
+
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#00ff00"
+            style={styles.loading}
+          />
+        ) : (
+          <FlatList
+            data={articles}
+            renderItem={() => <BottomNews navigation={navigation} />}
+            pagingEnabled
+          />
+        )}
       </View>
     </SafeAreaView>
   );
@@ -51,9 +63,14 @@ const styles = StyleSheet.create({
     paddingTop: "5%",
     flex: 1,
   },
-  text:{
+  text: {
     fontSize: 18,
     fontWeight: "bold",
     paddingLeft: 20,
-  }
+  },
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- screens/Home.js
+++ screens/Home.js
@@ -1,37 +1,48 @@
 import axios from "../helpers/axios";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BottomNews from "../components/BottomNews";
 import Categories from "../components/Categories/Categories";
 import LatestNews from "../components/LatestNews";
 import SearchBar from "../components/SearchBar";
 
-const Home = ({navigation}) => {
-
+const Home = ({ navigation }) => {
   const [activeTab, setActiveTab] = useState("");
   const [search, setSearch] = useState("");
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getSearchResults = () => {
+    setLoading(true);
     axios
       .get("api/article/searchLatest?search=" + search)
       .then((response) => setArticles(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getSearchResults();
-  },[])
-
+  }, []);
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <SearchBar search={search} setSearch={setSearch} handleClick={getSearchResults}  />
-        <LatestNews articles={articles} navigation={navigation} />
-        <Categories activeTab={activeTab} setActiveTab={setActiveTab} />
-        <BottomNews activeTab={activeTab} navigation={navigation} />
+        <SearchBar
+          search={search}
+          setSearch={setSearch}
+          handleClick={getSearchResults}
+        />
+        {loading ? (
+          <ActivityIndicator size="large" color="#00ff00" style={styles.loading} />
+        ) : (
+          <>
+            <LatestNews articles={articles} navigation={navigation} />
+            <Categories activeTab={activeTab} setActiveTab={setActiveTab} />
+            <BottomNews activeTab={activeTab} navigation={navigation} />
+          </>
+        )}
         {/* Bottom Nav */}
       </View>
     </SafeAreaView>
@@ -41,11 +52,16 @@ const Home = ({navigation}) => {
 export default Home;
 
 const styles = StyleSheet.create({
-  safeArea:{
-    flex:1,
+  safeArea: {
+    flex: 1,
   },
   container: {
     paddingTop: "5%",
-    flex:1,
+    flex: 1,
   },
+  loading:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
